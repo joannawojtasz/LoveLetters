437a71feef7ddda33263c101154ab0586f1d84ff
joannawojtasz <joannawojtasz@gmail.com>
2021-07-04 11:47:44 +0000

    Add display function for easy game

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,10 +26,9 @@ form.addEventListener("submit", startGame);
 
 function startGame(event) {
     event.preventDefault();
-    let data = getFormData();
-    // data = [username, difficulty, color]
+    let data = getFormData(); // data = [username, difficulty, color]
     document.getElementById("form-modal").style.display = "none";
-    let username =  document.getElementById("username");
+    let username = document.getElementById("username");
     username.innerHTML = data[0].toUpperCase();
     username.style.color = data[2];
     runGame(data[1]);
@@ -41,6 +40,7 @@ function getFormData() {
     let color = getColor(event);
     return [username, difficulty, color];
 }
+
 function getAge() {
     let ages = document.getElementsByClassName("age-input");
     for (let age of ages) {
@@ -48,8 +48,7 @@ function getAge() {
             let value = age.value;
             return value;
             break;
-        } else {
-        }
+        } else {}
     }
 }
 
@@ -60,23 +59,79 @@ function getColor() {
             let value = color.value;
             return value;
             break;
-        } else {
-        }
+        } else {}
     }
 }
 
 function runGame(difficulty) {
+    displayQuestion(difficulty);
     if (difficulty == 'easy') {
         runEasyGame()
     } else if (difficulty == 'medium') {
         runMediumGame()
-    } 
+    }
+}
+
+function displayQuestion(difficulty) {
+    if (difficulty == "easy") {
+        // let n = Math.floor(Math.random() * 20);
+        document.getElementById("question").src = question.easy[0].image;
+
+        let answers = document.getElementsByClassName('answer');
+        let correctAnswer = question.easy[0].name.charAt(0).toUpperCase();
+        for (answer of answers) {
+            answer.innerHTML = ''
+            let letters = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', ];
+            let letter = letters[Math.floor(Math.random() * 26)];
+            if (letter != correctAnswer) {
+                answer.innerHTML = letter;
+            } else {
+                continue;
+            }
+        }
+        answers[Math.floor(Math.random() * 3)].innerHTML = correctAnswer;
+    } else if (difficulty == "medium") {
+        console.log('this function will display question')
+    }
+
 }
 
 function runEasyGame() {
-    alert('Easy game not yet implemented')
+    let answer = listenForAnswer(easy);
+    let result = checkAnswer(answer);
+    incrementScores(result);
 }
 
 function runMediumGame() {
     alert('Medium game not yet implemented')
+}
+
+
+
+function listenForAnswer() {
+    console.log('this function will listen for users answer')
+}
+
+function checkAnswer(answer) {
+    console.log('this function will check for result')
+    let correctAnswer = question.easy[0].name.charAt(0).toUpperCase();
+}
+
+function incrementScores(result) {
+    console.log("result unknown")
+}
+
+let question = {};
+question = {
+    easy: [{
+            image: 'assets/images/question.png',
+            name: 'question',
+        },
+        // {
+        //     image: 'assets/images/,
+        //     name: ,
+        // },
+
+
+    ]
 }
