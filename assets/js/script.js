a5c22934e2d9acd323afb6398ecf70a01d927376
joannawojtasz <joannawojtasz@gmail.com>
2021-07-04 16:36:47 +0000

    Add additional questions and visual counters for results

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,10 +20,11 @@ document.getElementsByClassName("close")[1].onclick = function () {
     document.getElementById("form-modal").style.display = "none";
 }
 
-document.getElementsByTagName('form')[0].addEventListener("submit", startGame);
+document.getElementsByTagName('form')[0].addEventListener("submit", play);
+/////////////////////////////////////////////////////////////////////////////////////////////////
 
-function startGame(event) {
-     event.preventDefault();
+function play(event) {
+    event.preventDefault();
     let data = getFormData(); // data = [username, difficulty, color]
     document.getElementById("form-modal").style.display = "none";
     let username = document.getElementById("username");
@@ -62,11 +63,13 @@ function getColor() {
 }
 
 let score = 0;
-let questionNo = 1;
+let questionNo = 0;
 let lifes = 3;
 
 
+
 function runGame(difficulty) {
+    let correctAnswer = '';
     displayQuestion(difficulty);
     if (difficulty == 'easy') {
         runEasyGame()
@@ -76,15 +79,23 @@ function runGame(difficulty) {
 }
 
 function displayQuestion(difficulty) {
+    questionNo++
+    circelesColor()
     if (difficulty == "easy") {
-        // let n = Math.floor(Math.random() * 20);
-        document.getElementById("question").src = question.easy[0].image;
+        let n = Math.floor(Math.random() * 3);
+
+        document.getElementById("question").src = question.easy[n].image;
 
         let answers = document.getElementsByClassName('answer');
-        let correctAnswer = question.easy[0].name.charAt(0).toUpperCase();
+        correctAnswer = question.easy[n].name.charAt(0).toUpperCase();
         for (answer of answers) {
+            answer.style.backgroundColor = 'white';
+            answer.style.color = 'black';
             answer.innerHTML = ''
-            let letters = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', ];
+            let letters = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I',
+                'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L',
+                'Z', 'X', 'C', 'V', 'B', 'N', 'M',
+            ];
             let letter = letters[Math.floor(Math.random() * 26)];
             if (letter != correctAnswer) {
                 answer.innerHTML = letter;
@@ -101,9 +112,10 @@ function displayQuestion(difficulty) {
 function runEasyGame() {
     let answers = document.getElementsByClassName('answer');
     for (answer of answers) {
-    answer.addEventListener('click', function(){
-        checkAnswer(this);
-    })}
+        answer.addEventListener('click', function () {
+            checkAnswer(this);
+        })
+    }
     // let choosenAnswer = listenForAnswer(easy);
     //
     // let result = checkAnswer(choosenAnswer);
@@ -114,26 +126,20 @@ function runMediumGame() {
     alert('Medium game not yet implemented')
 }
 
-// function listenForAnswer() {
-
-// return choosenAnswer;
-// }
-
 function checkAnswer(answer) {
-    let correctAnswer = question.easy[0].name.charAt(0).toUpperCase();
     if (answer.innerHTML == correctAnswer) {
         answer.style.backgroundColor = 'green';
         answer.style.color = 'white';
         score++;
-        questionNo++
-        runEasyGame()
+        setTimeout(runGame, 300, 'easy');
+
     } else {
         answer.style.backgroundColor = 'red';
         answer.style.color = 'white';
-        questionNo++
-        runEasyGame()
+
+        setTimeout(runGame, 300, 'easy');
         lifes -= 1;
-    }  
+    }
 }
 
 function incrementScores(result) {
@@ -146,11 +152,33 @@ question = {
             image: 'assets/images/question.png',
             name: 'question',
         },
-        // {
-        //     image: 'assets/images/,
-        //     name: ,
-        // },
+        {
+            image: 'assets/images/bee.png',
+            name: 'bee',
+        },
+        {
+            image: 'assets/images/snowman.png',
+            name: 'snowman',
+        },
 
 
     ]
+}
+
+function circelesColor() {
+    let questionCount = document.getElementsByClassName('qcount');
+    let lifesCount = document.getElementsByClassName('lcount');
+    for (let i = 0; i < 10; i++ ) {
+        questionCount[i].style.backgroundColor = 'white'; 
+     }
+    for (let i = 0; i < questionNo; i++ ) {
+        questionCount[i].style.backgroundColor = 'green'; 
+     }
+     for (let i = 0; i < 3; i++ ) {
+        lifesCount[i].style.backgroundColor = 'white'; 
+     }
+    for (let i = 0; i < lifes; i++ ) {
+        lifesCount[i].style.backgroundColor = 'red'; 
+     }
+
 }
