733377b62daa6faec252dc00c6c716e5cacfd241
joannawojtasz <joannawojtasz@gmail.com>
2021-07-07 20:04:20 +0000

    add close to form modal on index page

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,12 @@
 document.getElementsByClassName("pulse-button")[0].onclick = function () {
     document.getElementById("form-modal").style.display = "block";
 };
-
+let closeButtons = document.getElementsByClassName("close");
+for (closebutton of closeButtons) {
+    closebutton.onclick = function () {
+    document.getElementById("rules-modal").style.display = "none";
+};
+}
 document.getElementById("rules").onclick = function () {
     document.getElementById("rules-modal").style.display = "block";
 };
