573c4105bda57b6cd61fd0f6f7a83702b90a856b
joannawojtasz <joannawojtasz@gmail.com>
2021-07-07 18:21:29 +0000

    edit play button at  start page

--- assets/js/start.js
+++ assets/js/start.js
@@ -2,8 +2,6 @@ document.getElementsByClassName("play")[0].onclick = function () {
     document.getElementById("form-modal").style.display = "block";
 };
 
-document.getElementsByTagName('form')[0].addEventListener("submit", saveData);
-
 document.getElementById("rules").onclick = function () {
     document.getElementById("rules-modal").style.display = "block";
 };
